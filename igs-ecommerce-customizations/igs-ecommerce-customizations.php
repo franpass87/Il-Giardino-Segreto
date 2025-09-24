73dd8541dd1153dd50dc5c088f8e82c4e3f78669
franpass87 <francesco.passeri@gmail.com>
2025-09-24 12:09:18 +0200

    Replace plugin architecture with requested WooCommerce customizations

--- igs-ecommerce-customizations/igs-ecommerce-customizations.php
+++ igs-ecommerce-customizations/igs-ecommerce-customizations.php
@@ -3,7 +3,7 @@
  * Plugin Name:       IGS Ecommerce Customizations
  * Plugin URI:        https://example.com/
  * Description:       Raccolta di personalizzazioni per Il Giardino Segreto su WooCommerce.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Il Giardino Segreto
  * Author URI:        https://example.com/
  * Text Domain:       igs-ecommerce
@@ -14,13 +14,4 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'IGS_ECOMMERCE_VERSION', '1.0.0' );
-define( 'IGS_ECOMMERCE_PLUGIN_FILE', __FILE__ );
-define( 'IGS_ECOMMERCE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'IGS_ECOMMERCE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
-require_once IGS_ECOMMERCE_PLUGIN_DIR . 'includes/class-igs-plugin.php';
-
-add_action( 'plugins_loaded', static function () {
-    \IGS_Ecommerce_Customizations\Plugin::instance()->init();
-} );
+require_once __DIR__ . '/includes/snippets.php';
